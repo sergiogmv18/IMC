6900635433c7a0d6cb009a20caace831af6bf091
sergiogmv18 <sergiogmv18@gmail.com>
2023-08-24 20:25:25 -0300

    Initial project

---
+++ bin/calculator_imc.dart
@@ -0,0 +1,5 @@
+import 'package:calculator_imc/calculator_imc.dart' as calculator_imc;
+
+void main(List<String> arguments) {
+  print('Hello world: ${calculator_imc.calculate()}!');
+}
---
+++ test/calculator_imc_test.dart
@@ -0,0 +1,8 @@
+import 'package:calculator_imc/calculator_imc.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
