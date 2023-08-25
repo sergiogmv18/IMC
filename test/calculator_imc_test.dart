2b766c35ea7a2ea0572c8ccccd8c11bb2fe25a56
sergio Giovanny Marcano Villahermosa <74307701+sergiogmv18@users.noreply.github.com>
2023-08-25 10:57:08 -0300

    Update calculator_imc_test.dart
    Create test of functions

--- test/calculator_imc_test.dart
+++ test/calculator_imc_test.dart
@@ -1,8 +1,14 @@
-import 'package:calculator_imc/main.dart';
+import 'package:calculator_imc/calculator.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('calculate', () {
-   // expect(calculate(), 42);
+    expect(BMICalculator.calculate(weightCurrent:73, heightCurrent: 176).toStringAsFixed(1), '23.6');
+  });
+  test('show estado: 1', () {
+    expect(BMICalculator.classificationBmi(32), 'Obesidade Grau I');
+  });
+  test('show estado: 2', () {
+    expect(BMICalculator.classificationBmi(377.16), 'Obesidade Grau III (moderada)');
   });
 }
